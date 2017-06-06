7eb9491d010d870902e9e813b812dca6b3c50b64
NouiliKh <khalil.nouili@gmail.com>
2017-06-06 13:35:08 +0100

    Snow

--- explotions/Assets/objectground.cs
+++ explotions/Assets/objectground.cs
@@ -7,21 +7,23 @@ public class objectground : MonoBehaviour
     public GameObject explosion;
     public GameObject explosionground;
     public GameObject groundPound;
+    public GameObject rockchunk;
 
 	// Use this for initialization
 	void Start () {
-		
+       
 
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             Instantiate(explosion, transform.position, explosion.transform.rotation);
             Instantiate(explosionground, transform.position, explosion.transform.rotation);
+            Instantiate(rockchunk, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), rockchunk.transform.rotation);
         }
-	}
+    }
 
     private void OnCollisionEnter(Collision collision)
     {

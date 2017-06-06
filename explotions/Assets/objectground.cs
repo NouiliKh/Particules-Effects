0bc4a913ffee60012488bd5372a74953787cfbea
NouiliKh <khalil.nouili@gmail.com>
2017-06-06 12:33:18 +0100

    explosions and ground effect

---
+++ explotions/Assets/objectground.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectground : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject explosionground;
+    public GameObject groundPound;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(explosion, transform.position, explosion.transform.rotation);
+            Instantiate(explosionground, transform.position, explosion.transform.rotation);
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(groundPound, transform.position, groundPound.transform.rotation);
+    }
+
+}
+
+
